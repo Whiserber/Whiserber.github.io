5bb717b82a672d37f9d164e473dbb060eee16a31
Whiserber <129470051+Whiserber@users.noreply.github.com>
2023-04-02 18:44:22 -0400

    Add files via upload

---
+++ js/theme.js
@@ -0,0 +1,18 @@
+const preferedColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+const slider = document.getElementById('checkbox');
+
+const setTheme = (theme) => {
+    document.documentElement.setAttribute('data-theme',theme);
+    localStorage.setItem('theme', theme);
+}
+
+slider.addEventListener('click', () => {
+    let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+    setTheme(switchToTheme);
+});
+
+setTheme(localStorage.getItem('theme') || preferedColorScheme);
+
+if (localStorage.getItem('theme') === 'dark'){
+    slider.checked = true;
+}
