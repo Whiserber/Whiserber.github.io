d9d05f7c9609dc263d921f0315aba863feb49a11
Whiserber <whiserber@gmail.com>
2023-04-04 16:14:14 -0400

    Update: mover carpetar a carpeta "src"

---
+++ Aqours/conciertos/src/js/windy.js
@@ -0,0 +1,113 @@
+var serverSelect = document.getElementById('select_server');
+var daySelect = document.getElementById('select_day');
+var partSelect = document.getElementById('select_part');
+var DDEButton = document.getElementById('DDE');
+var videoIframe = document.getElementById('video_iframe');
+
+videoIframe.src = "https://filemoon.sx/e/6klo78jselhu/Aqours_6th_Lovelive!__Ku-Ru-Ku-Ru_Rock__N__Roll_Tour___Windy_Stage__Dia_1_Parte_1";
+
+serverSelect.addEventListener('change', updateVideo);
+daySelect.addEventListener('change', updateVideo);
+partSelect.addEventListener('change', updateVideo);
+
+
+function updateVideo(){
+    var serverValue = serverSelect.value;
+    var dayValue = daySelect.value;
+    var partValue = partSelect.value;
+    if(serverValue == "S1")
+    {
+        if(dayValue == "D1"){
+            if(partValue == "P1"){
+                videoIframe.src = "https://filemoon.sx/e/6klo78jselhu/Aqours_6th_Lovelive!__Ku-Ru-Ku-Ru_Rock__N__Roll_Tour___Windy_Stage__Dia_1_Parte_1";
+            }
+            else if(partValue == "P2")
+            {
+                videoIframe.src = "https://filemoon.sx/e/tyr8jn93i2sq/Aqours_6th_Lovelive!__Ku-Ru-Ku-Ru_Rock__N__Roll_Tour___Windy_Stage__Dia_1_Parte_2";
+            }
+            else
+            {
+                videoIframe.src = "https://filemoon.sx/e/4jdyg83hddsa/Making_Of_Aqours_6th_Lovelive!__Ku-Ru-Ku-Ru_Rock__N__Roll_Tour___Ocean_Stage___SubEsp_";
+            }
+        }
+        else
+        {
+            
+            if(partValue == "P1"){
+                videoIframe.src = "https://filemoon.sx/e/g2jjp5d7jzs5/Aqours_6th_Lovelive!__Ku-Ru-Ku-Ru_Rock__N__Roll_Tour___Windy_Stage__Dia_2_Parte_1";
+            }
+            else if(partValue == "P2")
+            {
+                videoIframe.src = "https://filemoon.sx/e/t4tl8ihhac02/Aqours_6th_Lovelive!__Ku-Ru-Ku-Ru_Rock__N__Roll_Tour___Windy_Stage__Dia_2_Parte_2";
+            }
+            else
+            {
+                videoIframe.src = "https://filemoon.sx/e/4jdyg83hddsa/Making_Of_Aqours_6th_Lovelive!__Ku-Ru-Ku-Ru_Rock__N__Roll_Tour___Ocean_Stage___SubEsp_";
+            }
+        }
+    }
+    else if(serverValue == "S2")
+    {
+        if(dayValue == "D1"){
+            if(partValue == "P1"){
+                videoIframe.src = "https://guccihide.com/e/5jtuska6kvqu";
+            }
+            else if(partValue == "P2")
+            {
+                videoIframe.src = "https://guccihide.com/e/xl8e80pgoofz";
+            }
+            else
+            {
+                videoIframe.src = "https://filemoon.sx/e/4jdyg83hddsa/Making_Of_Aqours_6th_Lovelive!__Ku-Ru-Ku-Ru_Rock__N__Roll_Tour___Ocean_Stage___SubEsp_";
+            }
+        }
+        else
+        {
+            
+            if(partValue == "P1"){
+                videoIframe.src = "https://guccihide.com/e/b6ejrrxvjldv";
+            }
+            else if(partValue == "P2")
+            {
+                videoIframe.src = "https://guccihide.com/e/crv060qsaei0";
+            }
+            else
+            {
+                videoIframe.src = "https://filemoon.sx/e/4jdyg83hddsa/Making_Of_Aqours_6th_Lovelive!__Ku-Ru-Ku-Ru_Rock__N__Roll_Tour___Ocean_Stage___SubEsp_";
+            }
+        }
+    }
+    else
+    {
+        if(dayValue == "D1"){
+            if(partValue == "P1"){
+                videoIframe.src = "https://sbface.com/e/nwkmwyvf9ka6.html";
+            }
+            else if(partValue == "P2")
+            {
+                videoIframe.src = "https://sbface.com/e/ndusf26yc6km.html";
+            }
+            else
+            {
+                videoIframe.src = "https://filemoon.sx/e/4jdyg83hddsa/Making_Of_Aqours_6th_Lovelive!__Ku-Ru-Ku-Ru_Rock__N__Roll_Tour___Ocean_Stage___SubEsp_";
+            }
+        }
+        else
+        {
+            
+            if(partValue == "P1"){
+                videoIframe.src = "https://sbface.com/e/hlkpylrylny1.html";
+            }
+            else if(partValue == "P2")
+            {
+                videoIframe.src = "https://sbface.com/e/ndap8lrakj3b.html";
+            }
+            else
+            {
+                videoIframe.src = "https://filemoon.sx/e/4jdyg83hddsa/Making_Of_Aqours_6th_Lovelive!__Ku-Ru-Ku-Ru_Rock__N__Roll_Tour___Ocean_Stage___SubEsp_";
+            }
+        }
+    }
+
+    
+}
