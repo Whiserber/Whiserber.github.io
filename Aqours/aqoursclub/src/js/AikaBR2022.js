5b1a2d8dba1132f2ab15309859b8da7834eb139b
Whiserber <whiserber@gmail.com>
2023-04-07 16:34:20 -0400

    Valentine concert agregado

---
+++ Aqours/aqoursclub/src/js/AikaBR2022.js
@@ -0,0 +1,25 @@
+var serverSelect = document.getElementById('select_server');
+var videoIframe = document.getElementById('video_iframe');
+
+videoIframe.src = "https://filemoon.sx/e/c872q95u6nf6/Aika_Kobayashi_Birthday_Report__SubEsp_";
+
+serverSelect.addEventListener('change', updateVideo);
+
+
+function updateVideo(){
+    var serverValue = serverSelect.value;
+    if(serverValue == "S1")
+    {
+        videoIframe.src = "https://filemoon.sx/e/c872q95u6nf6/Aika_Kobayashi_Birthday_Report__SubEsp_";
+
+    }
+    else if(serverValue == "S2")
+    {
+        videoIframe.src = "https://guccihide.com/e/csdycjad5ppp";
+    }
+    else
+    {
+        videoIframe.src = "https://sbface.com/e/lmictb7lnwf8";
+    }
+    
+}
--- Aqours/seiyuus/src/js/adolescence.js
+++ Aqours/seiyuus/src/js/adolescence.js
@@ -6,21 +6,20 @@ videoIframe.src = "https://filemoon.sx/e/c872q95u6nf6/Aika_Kobayashi_Birthday_Re
 serverSelect.addEventListener('change', updateVideo);
 
 
-
 function updateVideo(){
     var serverValue = serverSelect.value;
-
     if(serverValue == "S1")
     {
-        videoIframe.src = "https://filemoon.sx/e/c872q95u6nf6/Aika_Kobayashi_Birthday_Report__SubEsp_";
+        videoIframe.src = "https://filemoon.sx/e/wha65hf0gmq6/Aida_Rikako__Adolescence_";
+
     }
     else if(serverValue == "S2")
     {
-        videoIframe.src = "https://filemoon.sx/e/c872q95u6nf6/Aika_Kobayashi_Birthday_Report__SubEsp_";
+        videoIframe.src = "https://guccihide.com/e/f30wrucg3ede";
     }
     else
     {
-        videoIframe.src = "https://filemoon.sx/e/c872q95u6nf6/Aika_Kobayashi_Birthday_Report__SubEsp_";
+        videoIframe.src = "https://sbface.com/e/jkhvjlwh4r84";
     }
     
 }
