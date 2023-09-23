1e1f52db34fb06a015a56cd19431c48aa9f72726
Whiserber <whiserber@gmail.com>
2023-09-23 20:41:10 -0300

    GK 2nd

--- aqours/conciertos/src/js/returntolove.js
+++ aqours/conciertos/src/js/returntolove.js
@@ -3,7 +3,7 @@ var daySelect = document.getElementById('select_day');
 var partSelect = document.getElementById('select_part');
 var videoIframe = document.getElementById('video_iframe');
 
-videoIframe.src = "https://filemoon.sx/e/k90uen4xz6gg/Guilty_Kiss_2nd_LoveLive!__Return_To_Love___Kiss_Kiss_Kiss__D_a_1_Parte_1";
+videoIframe.src = "https://filemoon.sx/e/zie5yli8ohms";
 
 serverSelect.addEventListener('change', updateVideo);
 daySelect.addEventListener('change', updateVideo);
@@ -25,81 +25,19 @@ function updateVideo(){
                 videoIframe.src = "https://filemoon.sx/e/e3sloz7ial2e/Guilty_Kiss_2nd_LoveLive!__Return_To_Love___Kiss_Kiss_Kiss__D_a_1_Parte_2";
             }
         }
-        /*else
+        else
         {
-            
             if(partValue == "P1"){
-                videoIframe.src = "";
+                videoIframe.src = "https://filemoon.sx/e/zie5yli8ohms";
             }
             else if(partValue == "P2")
             {
-                videoIframe.src = "";
+                videoIframe.src = "https://filemoon.sx/e/y8vnddt3nfoo";
             }
             else
             {
-                videoIframe.src = "";
+                videoIframe.src = "https://filemoon.sx/e/pslgvnfblfk2";
             }
-        }*/
-    }
-    else if(serverValue == "S2")
-    {
-        if(dayValue == "D1"){
-            if(partValue == "P1"){
-                videoIframe.src = "https://guccihide.com/e/x8dgyqeapnxz";
-            }
-            else if(partValue == "P2")
-            {
-                videoIframe.src = "https://guccihide.com/e/hkyycmp7cboa";
-            }
-
         }
-        /*else
-        {
-            
-            if(partValue == "P1"){
-                videoIframe.src = "";
-            }
-            else if(partValue == "P2")
-            {
-                videoIframe.src = "";
-            }
-            else
-            {
-                videoIframe.src = "";
-            }
-        }*/
     }
-    else
-    {
-        if(dayValue == "D1"){
-            if(partValue == "P1"){
-                videoIframe.src = "https://sbface.com/e/suae0qv5v34a.html";
-            }
-            else if(partValue == "P2")
-            {
-                videoIframe.src = "https://sbface.com/e/t85kvo7dhyvq.html";
-            }
-            /*else
-            {
-                videoIframe.src = "https://sbface.com/e/ete2hslbq53r.html";
-            }*/
-        }
-        /*else
-        {
-            
-            if(partValue == "P1"){
-                videoIframe.src = "";
-            }
-            else if(partValue == "P2")
-            {
-                videoIframe.src = "";
-            }
-            else
-            {
-                videoIframe.src = "";
-            }
-        }*/
-    }
-
-    
 }
