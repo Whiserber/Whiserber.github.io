375ecbdd54615ed74b8dc28e42813f4927be7f6d
Diego González <diego.gonzalez07@alumnos.ucn.cl>
2024-07-14 19:07:34 -0400

    fix lost world js

--- aqours/conciertos/src/js/lostworld.js
+++ aqours/conciertos/src/js/lostworld.js
@@ -1,19 +1,18 @@
 var serverSelect = document.getElementById('select_server');
 var daySelect = document.getElementById('select_day');
-var partSelect = document.getElementById('select_part');
 var videoIframe = document.getElementById('video_iframe');
 
 videoIframe.src = "https://filemoon.sx/e/cyqsa1zeokzp";
 
 serverSelect.addEventListener('change', updateVideo);
 daySelect.addEventListener('change', updateVideo);
-partSelect.addEventListener('change', updateVideo);
+
 
 
 function updateVideo(){
     var serverValue = serverSelect.value;
     var dayValue = daySelect.value;
-    var partValue = partSelect.value;
+
     if(serverValue == "S1")
     {
         if(dayValue == "D1"){
